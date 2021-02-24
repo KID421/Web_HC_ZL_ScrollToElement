4243230a7ebfde3eefb72156b1a5a4900a81808d
KID421 <suchild7@gmail.com>
2021-02-24 21:04:00 +0800

    取得指定元素上方位置

--- main.js
+++ main.js
@@ -9,5 +9,9 @@ $("a").each(function (index, element) {
 
     console.log("目標：" + target);
     console.log("時間：" + duration);
-    
+
+    var offset = $("#" + target).offset();
+    var top = offset.top;
+
+    console.log("上方：" + top);
 });
