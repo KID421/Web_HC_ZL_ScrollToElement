dbfbcae8a5eb940c5a6686ae8094e0c554cee8e0
KID421 <suchild7@gmail.com>
2021-02-24 20:50:25 +0800

    取得元素屬性資料

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-$(selector).each(function (index, element) {
-    // element == this
+// 搜尋 a 元素
+// 取得每個 a 元素的資料
+$("a").each(function (index, element) {
+    
+    console.log("編號：" + index);
+
+    var target = $(this).attr("data-st-target");
+    var duration = $(this).attr("data-st-duration");
+
+    console.log("目標：" + target);
+    console.log("時間：" + duration);
     
 });
