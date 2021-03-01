d10b49e57488b725ddcdc53a904852868c42a4ca
KID421 <suchild7@gmail.com>
2021-03-01 18:58:23 +0800

    添加位移參數

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ $("a").each(function (index, element) {
 
     var target = $(this).attr("data-st-target");
     var duration = $(this).attr("data-st-duration");
+    var offsetTop = $(this).attr("data-st-offset");
 
     console.log("目標：" + target);
     console.log("時間：" + duration);
+    console.log("位移：" + offsetTop);
 
     var offset = $("#" + target).offset();
     var top = offset.top;
+
+    // 上方 扣除 上方位移
+    // top = top - offsetTop; // 原始寫法
+    // 如果 offsetTop 存在的話 才執行 減法運算
+    if (offsetTop) top -= offsetTop;         // 簡寫2
 
     console.log("上方：" + top);
 
